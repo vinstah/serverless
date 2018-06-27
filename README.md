be8f970ddd2f8f30a6ac3e8ff0179c9ac0600b40
Vinnie <vinnie.watson@ipayroll.co.nz>
2018-06-27 14:11:59 +1200

    refactor user data stored in json file and update express to allow for /users/:userId request

--- README.md
+++ README.md
@@ -2,14 +2,23 @@
 
 ## Requirements
 
+node v8
+AWS acount credentials [aws free](https://aws.amazon.com/free/#AWS_FREE_TIER)
+serverless config credentials --provider aws --key {AWS KEY} --secret {AWS SECRET}
+
+### Set up
+
 ``` bash
 npm i serverless -g
-npm i
+
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 echo $PATH | grep ~/bin
 export PATH=~/bin:$PATH
+
+npm i
 ```
 
 To ensure that your $PATH settings are retained between sessions, add the export line to your shell profile (~/.profile, ~/.bash_profile, etc).
+[quick tips for faster serverless development](https://serverless.com/blog/quick-tips-for-faster-serverless-development/)
--- index.js
+++ index.js
@@ -4,8 +4,39 @@
  *
  */
 
+const serverless = require('serverless-http');
+const express = require('express');
+const app = express();
+
+// http://json-schema-faker.js.org/
+// InnerReferences example
+const userData = require('./data/users.json');
+
+// Default page
+app.get('/', (req, res) => {
+  res.send(`<h1>Users API</h1><div><a href="users">View Api</a></div>`);
+});
+
+// localhost:3000/users/
+app.get('/users', (req, res) => {
+  res.json(userData);
+});
+
+// localhost:3000/users/62156
+app.get('/users/:userId', function (req, res) {
+  let requestedUser = userData.find(obj => {return obj.id === parseInt(req.params.userId, 10)});
+
+  if (typeof requestedUser === 'object') {
+    res.json(requestedUser)
+  } else {
+    res.status(404).json({ error: "User not found" })
+  }
+})
+
+module.exports.handler = serverless(app);
+
 // Block for function that works without express ( if you remove the app.get('/user') block of code)
-// Using express means you only need to push one function to AWS lambda
+// Using express allows you only need to push one function to AWS lambda
 /*
 module.exports.user = (event, context, callback) => {
     const response = {
@@ -22,21 +53,4 @@ module.exports.user = (event, context, callback) => {
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
   };
-  */
-
-const serverless = require('serverless-http');
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Users API</h1><div><a href="users">View Api</a></div>`);
-});
-
-app.get('/users', (req, res) => {
-    res.send(JSON.stringify({
-        id: 1222,
-        name: 'Clare Pickle',
-        job: 'Earth Digger'
-      }));
-});
-module.exports.handler = serverless(app);
+*/
